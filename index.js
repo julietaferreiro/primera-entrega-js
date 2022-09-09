82d4743005ac4c236ff492c8aa144196a526e2f7
julieta fereiro <107197852+julietaferreiro@users.noreply.github.com>
2022-09-09 10:18:04 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("Entrega 01 JavaScript CoderHouse");
+
+
+
+let respuesta = prompt("Bienvenido a mi bar, que edad tenes?")
+
+if (respuesta > 17) {
+    alert("Sos mayor de 18, tomate una pinta campeon, Yo invito la primera!!!!");
+} else if (respuesta == 17) {
+    alert("casi casi, tomate una pinta sin alcohol, para tu cumple te regalo una ipa!");
+}
+else {
+    alert("Menos de 18?!!!, Mejor tomate una lechita!")
+}
